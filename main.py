cc84c001eb9310e591b36e9051b1b34ec3f864d4
[Jeramiah Coffey] <[jerrygcoffey@gmail.com]>
2021-08-11 17:57:59 -0500

    Added generate password functionality, which also copies the generated password to the clipboard. Bound <Return> key to add button.

--- main.py
+++ main.py
@@ -1,27 +1,65 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_pw():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    pw_letters = [random.choice(letters) for _ in range(nr_letters)]
+    pw_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    pw_nums = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = pw_letters + pw_symbols + pw_nums
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pw_entry.delete(0, END)
+    pw_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add():
+def add(event=None):
     website = website_entry.get()
     email = email_entry.get()
     pw = pw_entry.get()
 
-    with open("data.txt", mode="a") as data:
-        data.write(f"{website} | {email} | {pw}\n")
+    if website == "" or email == "" or pw == "":
+        messagebox.showwarning(message="Please complete all fields")
 
-    website_entry.delete(0, END)
-    email_entry.delete(0, END)
-    pw_entry.delete(0, END)
+    else:
+        is_ok = messagebox.askokcancel(title=website, message="These are the details entered: \n"
+                                                              f"Email: {email} \nPassword: {pw} \n"
+                                                              f"Is it okay to save?")
+
+        if is_ok:
+            with open("data.txt", mode="a") as data:
+                data.write(f"{website} | {email} | {pw}\n")
+
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            pw_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 app = Tk()
 app.title("Password Manager")
 app.config(padx=50, pady=50)
+app.minsize(width=100, height=100)
 app.eval('tk::PlaceWindow . center')
+app.bind('<Return>', add)
 
 logo_img = PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200, highlightthickness=0)
@@ -47,7 +85,7 @@ pw_label.grid(row=3, column=0, sticky=E)
 pw_entry = Entry(width=21)
 pw_entry.grid(row=3, column=1)
 
-gen_pw = Button(text="Generate Password")
+gen_pw = Button(text="Generate Password", command=generate_pw)
 gen_pw.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=add)
